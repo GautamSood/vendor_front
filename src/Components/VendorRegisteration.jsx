5da0ed18d8bc0a7a3b04af40121adead6f3bb5e1
GautamSood <gautamsood.cse19@chitkarauniversity.edu.in>
2023-05-14 01:17:54 +0530

    first commit

---
+++ src/Components/VendorRegisteration.jsx
@@ -0,0 +1,285 @@
+import React from 'react'
+import axios from "axios"
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const VendorRegisteration = () => {
+  
+  const formsubmit = (e) => {
+     e.preventDefault();
+      console.log("function is working")
+      let formData = new FormData(e.target);
+      formData = Object.fromEntries(formData);
+      console.log(formData);
+
+    axios.post("vendors/signUp", {
+        data: formData
+      })
+      .then((res) => {
+        console.log(res);
+        toast.success("hi");
+      })
+      .catch((err) => {
+        console.log(err.message);
+        toast.error("Registration failed");
+      });
+    };
+  return (
+    <div style={{ fontFamily: "serif" }}>
+      <h1 className="" style={{ textAlign: "center" }}>
+        Vendor Registeration
+      </h1>
+
+      <div className="container">
+        <button type="button" className="btn btn-outline-primary mb-5">
+          Home
+        </button>
+      </div>
+
+      <div className="container">
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            formsubmit(event);
+          }}
+        >
+          <div className="mb-3 row">
+            <div className="col">
+              <label htmlFor="NameOfTheCompany" className="form-label">
+                Name Of The Company / Firm
+              </label>
+              <input
+                className="form-control"
+                name="NameOfTheCompany"
+                aria-describedby="emailHelp"
+              />
+            </div>
+
+            <div className="col">
+              <label htmlFor="Contact Person Name" className="form-label">
+                Contact Person Name
+              </label>
+              <input
+                className="form-control"
+                name="ContactPersonName"
+                aria-describedby="emailHelp"
+              />
+            </div>
+          </div>
+
+          <div className="mb-3 row">
+            <div className="col">
+              <label htmlFor="PrimaryEmailID" className="form-label">
+                Primary Email ID
+              </label>
+              <input className="form-control" name="PrimaryEmailID" />
+            </div>
+            <div className="col">
+              <label htmlFor="Password" className="form-label">
+                Password
+              </label>
+              <input type="password" className="form-control" name="Password" />
+            </div>
+          </div>
+
+          <div className="mb-3 row">
+            <div className="col">
+              <label htmlFor="SecondaryEmailID" className="form-label">
+                Secondary Email ID
+              </label>
+              <input className="form-control" name="SecondaryEmailID" />
+            </div>
+            <div className="col"></div>
+          </div>
+
+          <div className="mb-3 row">
+            <div className="col">
+              <label htmlFor="Address" className="form-label">
+                Address
+              </label>
+              <input className="form-control" name="Address" />
+            </div>
+            <div className="col">
+              <label htmlFor="Street" className="form-label">
+                Street
+              </label>
+              <input className="form-control" name="Street" />
+            </div>
+          </div>
+
+          <div className="mb-3 row">
+            <div className="col">
+              <label htmlFor="State" className="form-label">
+                State
+              </label>
+              <input className="form-control" name="State" />
+            </div>
+            <div className="col">
+              <label htmlFor="PinCode" className="form-label">
+                Pin Code
+              </label>
+              <input className="form-control" name="PinCode" />
+            </div>
+          </div>
+
+          <div className="mb-3 row">
+            <div className="col">
+              <label htmlFor="PrimaryMobileNumber" className="form-label">
+                Primary Mobile Number
+              </label>
+              <input className="form-control" name="PrimaryMobileNumber" />
+            </div>
+
+            <div className="col">
+              <label htmlFor="SecondaryMobileNumber" className="form-label">
+                Secondary Mobile Number
+              </label>
+              <input className="form-control" name="SecondaryMobileNumber" />
+            </div>
+          </div>
+
+          <div className="mb-3 row">
+            <div className="col">
+              <label htmlFor="BankName" className="form-label">
+                Bank Name
+              </label>
+              <input className="form-control" name="BankName" />
+            </div>
+            <div className="col">
+              <label htmlFor="BankAddress" className="form-label">
+                Bank Address
+              </label>
+              <input className="form-control" name="BankAddress" />
+            </div>
+          </div>
+
+          <div className="mb-3 row">
+            <div className="col">
+              <label htmlFor="BankAccountNumber" className="form-label">
+                Bank Account Number
+              </label>
+              <input className="form-control" name="BankAccountNumber" />
+            </div>
+            <div className="col">
+              <label htmlFor="BankIFSCCode" className="form-label">
+                Bank IFSC Code
+              </label>
+              <input className="form-control" name="BankIFSCCode" />
+            </div>
+          </div>
+
+          <div className="mb-3 row">
+            <div className="col">
+              <label htmlFor="TypeOfVendor" className="form-label">
+                Type Of Vendor
+              </label>
+              <select
+                className="form-select"
+                name="TypeOfVendor"
+                defaultValue={"Courier"}
+              >
+                <option value="Courier">Courier</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+            </div>
+            <div className="col">
+              <label htmlFor="FrequencyBillSubmission" className="form-label">
+                Frequency Bill Submission
+              </label>
+              <select
+                className="form-select"
+                name="FrequencyBillSubmission"
+                defaultValue={"Recurring"}
+              >
+                <option value="Recurring">Recurring</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="mb-3 row">
+            <div className="col">
+              <label htmlFor="GSTInputCred" className="form-label">
+                GST Input Credit Eligibility
+              </label>
+              <select
+                className="form-select"
+                name="GSTInputCred"
+                defaultValue={"Courier"}
+              >
+                <option value="Courier">Courier</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+            </div>
+            <div className="col">
+              <label htmlFor="TDSApplicabilityType" className="form-label">
+                TDS Applicability Type
+              </label>
+              <select
+                className="form-select"
+                name="TDSApplicabilityType"
+                defaultValue={"Recurring"}
+              >
+                <option value="Recurring">Recurring</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="mb-3 row">
+            <div className="col">
+              <label htmlFor="LowerTDSCertificate" className="form-label">
+                Does vendor has lower TDS Certificate
+              </label>
+              <select
+                className="form-select"
+                name="LowerTDSCertificate"
+                defaultValue={"Courier"}
+              >
+                <option value="Courier">Courier</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+            </div>
+            <div className="col">
+              <label
+                htmlFor="LowerTaxDeductionCertificate"
+                className="form-label"
+              >
+                Lower Tax Deduction Certificate
+              </label>
+              <select
+                className="form-select"
+                name="LowerTaxDeductionCertificate"
+                defaultValue={"Recurring"}
+              >
+                <option value="Recurring">Recurring</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+            </div>
+          </div>
+
+          <h3>Details of Buyer</h3>
+
+
+          <button type="submit" className="btn btn-success">
+            submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default VendorRegisteration
